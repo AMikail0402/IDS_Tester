7702bc88ae4f3c01a2739b1dabc3ea7392acf9f5
AMikail0402 <alpay.yildiz@outlook.com>
2023-12-26 21:25:40 +0100

    Jenkinsfile +Refactoring

--- src/main/java/util/SocketFactory.java
+++ src/main/java/util/SocketFactory.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
 import java.net.UnknownHostException;
 
 
